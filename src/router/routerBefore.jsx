c664959ac5fd7e7f6472c785f3e06297baa5a65d
wujie <1475970138@qq.com>
2022-11-28 22:37:56 +0800

    dd

--- src/router/routerBefore.jsx
+++ src/router/routerBefore.jsx
@@ -1,12 +1,27 @@
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
 
-export default function Redirect(props) {
-  const navigate = useNavigate()
-  useEffect(() => {
-    // replace: true 代表的是要替换掉之前的页面
-    navigate(props.to, { replace: true })
+import { Navigate, useRoutes } from 'react-router-dom'
+import routes from './syncRouter'
+// 渲染路由
+const renderRoutes = (routes) => {
+  return routes.map((item) => {
+    const route = { meta: item.meta, path: item.path }
+    if (item.component) {
+      // element 要接收react.element类型 item.component 是对象 所以要转一下
+      route.element = <item.component />
+    }
+    if (item.children) {
+      route.children = renderRoutes(item.children)
+    }
+    if (item.redirect) {
+      route.element = <Navigate to={item.redirect} />
+    }
+    return route
   })
-  return null
+}
+
+export default function Router () {
+  // useRoutes API 把路由数组整合为 <Router> <Route path="xx" element="xxx"></Route>等 </Router>的路由组件  直接用于BrowserRouter中
+  console.log(renderRoutes(routes))
+  return useRoutes(renderRoutes(routes))
 }
 
